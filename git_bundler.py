94c57e3a80d2d862b7ead2b5ae1af0970553f715
JustinFung <835776509@qq.com>
2024-06-17 21:38:20 +0800

    Adding extract option to git bundler

--- git_bundler.py
+++ git_bundler.py
@@ -6,6 +6,7 @@ GIT_CMD_SUB    = " bundle create "
 GIT_CMD_END    = ".bundle --all"
 MAIN_REPO_PATH = os.getcwd()
 ROOT_PATH      = "".join(["/" + folder for folder in MAIN_REPO_PATH.split("/")[1:-1]])
+SPLIT          = "~!~"
 
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('-e', '--extract', default=None, help="Path to table and bundles")
@@ -42,7 +43,7 @@ def make_bundles():
 
         for module_path, module_rel_path in zip(modules, modules_rel):
             module_name     = module_path.split("/")[-1]
-            module_rel_path = "\n".join(module_rel_path.split("/"))
+            module_rel_path = SPLIT.join(module_rel_path.split("/"))
 
             print("\033[96m {}\033[00m" .format("\nBundling Repo: " + module_name))
             os.system(GIT_CMD_HEAD + module_path + GIT_CMD_SUB + module_name + GIT_CMD_END)
@@ -51,13 +52,27 @@ def make_bundles():
     else:
         Exception("Please attach this script to the root of your main repo")
 
-def extract_bundles():
-    print("Extract bundles not implemented")
-    pass
+def extract_bundles(bundles_path):
+    for bundle_name in os.listdir(bundles_path):
+        if ".bundle" in bundle_name:
+            bundle_rel_path = os.path.join(bundles_path, "/".join(bundle_name[:-7].split(SPLIT)))
+            print("\033[96m {}\033[00m" .format("\nExtracting Repo: " + bundle_rel_path))
+            os.system("git -C " + bundles_path + " clone " + bundle_name)
+
+    print(f"\033[96m\n\nCopying ...\033[00m")
+    for bundle_name in os.listdir(bundles_path):
+        if ".bundle" in bundle_name:
+            repo_chain = bundle_name[:-7].split(SPLIT)
+            bundle_rel_path = os.path.join(bundles_path, "/".join(repo_chain))
+            print(f"\033[96m{os.path.join(bundles_path, bundle_name)[:-7]} "
+                  f"-> {bundle_rel_path} \033[00m")
+
+            shutil.move(os.path.join(bundles_path, bundle_name)[:-7], os.path.join(bundles_path, repo_chain[-1]))
+            shutil.move(os.path.join(bundles_path, repo_chain[-1])+"/", bundle_rel_path)     
 
 if __name__ == "__main__":
     if ARGS.extract:
-        extract_bundles()
+        extract_bundles(ARGS.extract)
         print("Args: " + ARGS.extract)
 
     else:
