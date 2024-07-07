c0788ca6bf638072e34b6c030c16222986a6c29b
JustinFung <835776509@qq.com>
2024-07-07 13:50:46 +0800

    Add output interface

--- git_bundler.py
+++ git_bundler.py
@@ -5,11 +5,12 @@ A tiny scrip[t] I used to bundling and extracting large git repositories with su
 Only apply for Linux env
 
 '''
-import os, shutil, pickle, argparse
+import os, shutil, argparse
 
 ARGS = argparse.ArgumentParser()
-ARGS.add_argument('-b', '--bundle',  default=None, help="Path to the main repo root dir")
-ARGS.add_argument('-e', '--extract', default=None, help="Path to table and bundles")
+ARGS.add_argument('-b', '--bundle' , default=os.getcwd(), help="Path to the main repo root dir")
+ARGS.add_argument('-e', '--extract', default=None,        help="Path to bundles to extract")
+ARGS.add_argument('-o', '--output' , default=None,        help="Path to store output bundles")
 
 ARGS           = ARGS.parse_args()
 SPLIT          = "~!~"
@@ -18,21 +19,22 @@ GIT_CMD_BUNDLE = " bundle create "
 GIT_CMD_CLONE  = " clone "
 GIT_CMD_END    = ".bundle --all"
 
+
 def gitmodules_parser(root_path: str) -> [list, list]:
     repo_paths     = []
     repo_rel_paths = []
-    
+
     if ".git" not in os.listdir(root_path):
         return repo_paths, repo_rel_paths
-    
+
     repo_paths.append(root_path)
     repo_rel_paths.append("")
     print("\033[96m {}\033[00m" .format("Find main repo: " + root_path))    # Adding Main repository
-    
+
     if ".gitmodules" not in os.listdir(root_path):
         print("\033[96m {}\033[00m" .format("\nNo submodules"))
         return repo_paths, repo_rel_paths
-    
+
     with open(os.path.join(root_path, ".gitmodules"), "r") as f:
         lines = f.readlines()
         for idx, line in enumerate(lines):
@@ -45,20 +47,21 @@ def gitmodules_parser(root_path: str) -> [list, list]:
                 repo_paths.append(repo_path)                                # Adding Sub Repositories
                 print("\033[96m {}\033[00m" .format("Find sub repo: " + repo_path))
         f.close()
-
     return repo_paths, repo_rel_paths
 
 
 def make_bundles(main_repo_dir: str):
     main_repo_pardir = os.path.abspath(os.path.join(main_repo_dir, os.pardir))
     main_repo_name   = os.path.abspath(main_repo_dir).split("/")[-1]
-    
+
     module_paths, modules_rel_paths = gitmodules_parser(main_repo_dir)
     if len(module_paths) == 0 or len(modules_rel_paths) == 0:
         Exception("Please attach this script to the root of your main repo")
 
-    os.system("rm    -rf bundles")
-    os.system("mkdir -p  bundles")
+    output_dir = os.path.abspath(os.path.join(ARGS.output, "bundles")) if ARGS.output else os.path.join(main_repo_dir, "bundles")
+    os.system(f"rm    -rf {output_dir}")
+    os.system(f"mkdir -p  {output_dir}")
+
     for module_path, module_rel_path in zip(module_paths, modules_rel_paths):
         module_name     = module_path.split("/")[-1]
         bundle_path     = os.path.join(module_path, module_name + ".bundle")
@@ -68,7 +71,7 @@ def make_bundles(main_repo_dir: str):
         os.system(GIT_CMD_HEAD + module_path + GIT_CMD_BUNDLE + module_name + GIT_CMD_END)
         shutil.move(
             os.path.join(module_path, module_name + ".bundle"),
-            os.path.join(os.path.join(main_repo_dir, "bundles"), module_rel_name + ".bundle")
+            os.path.join(output_dir, module_rel_name + ".bundle")
         )   # Copy and bundles to bundle folder and rename with its path
     shutil.copy(os.getcwd() + "/git_bundler.py", main_repo_dir + "/bundles")
 
@@ -110,11 +113,8 @@ def extract_bundles(bundles_path: str):
 
 
 if __name__ == "__main__":
-    if ARGS.bundle:
-        make_bundles(ARGS.bundle)
-
-    elif ARGS.extract:
+    if ARGS.extract:
         extract_bundles(ARGS.extract)
 
     else:
-        make_bundles(os.getcwd())
+        make_bundles(ARGS.bundle)
